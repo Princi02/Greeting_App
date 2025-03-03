2550c36df965912abec148c09081b3cfed3b3d88
Princi02 <priyanshimangal891@gmail.com>
2025-03-03 23:02:17 +0530

    hello message completed

--- src/main/java/com/greeting/app/Greeting_App/GreetingAppApplication.java
+++ src/main/java/com/greeting/app/Greeting_App/GreetingAppApplication.java
@@ -2,12 +2,13 @@ package com.greeting.app.Greeting_App;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class GreetingAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GreetingAppApplication.class, args);
+		System.out.println("Greetings");
 	}
-
 }
